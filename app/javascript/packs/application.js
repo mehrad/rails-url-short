d0f8977498b7b315db4e7ddf2892a4404669c978
Mahrad <hossein.ataeefard@gmail.com>
2020-11-28 23:26:15 +0330

    Add webpack react first step

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require('./hello_react')
+require('bootstrap')
+import 'bootstrap/dist/css/bootstrap'
